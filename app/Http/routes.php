a12b4656645d651f1cfce3dd7271d4cdfbfb3c30
panboxiaosa <864633261@qq.com>
2015-06-10 14:55:15 +0800

    debug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,29 +12,16 @@
 */
 
 Route::get('/', 'WelcomeController@index');
-
 Route::post('register',['uses'=>'MateController@register']);
-
 Route::post('login',['uses'=>'MateController@login']);
-
 Route::get('recommend',[ 'uses'=>'PostController@recommend']);
-
 Route::get('search',['uses'=>'PostController@search']);
-
 Route::get('personal',['uses'=>'PostController@personal']);
-
 Route::get('detail',['uses'=>'PostController@detail']);
-
 Route::post('publish', ['middleware'=>'mate', 'uses'=>'PostController@publish']);
-
 Route::post('finish', ['middleware'=>'mate', 'uses'=>'TradeController@finish']);
-
 Route::get('online', ['middleware'=>'mate', 'uses'=>'MessageController@offlineMessage']);
-
 Route::post('chat', ['middleware'=>'mate', 'uses'=>'MessageController@chat']);
-
 Route::post('interest', ['middleware'=>'mate', 'uses'=>'TradeController@interest']);
-
 Route::get('info', ['uses'=>'MateController@info']);
-
 Route::get('activate', ['uses'=>'MateController@active']);
