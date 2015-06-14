5d620e32efcdfc54815b7f1c7394bf24bf6354fe
panboxiaosa <864633261@qq.com>
2015-06-14 21:36:41 +0800

    index

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1>{{$nickname}}同学</h1>
-        <h2>请点击这个链接激活账号：{{'http://www.hereprovides.com/passer/public/activate?id='.$id.'&code='.$activecode}}</h2>
+        <h2>请点击这个链接激活账号：{{'http://www.hereprovides.com/passer/public/index.php/activate?id='.$id.'&code='.$activecode}}</h2>
         <p style="text-align: right;">Enjoy yourself</p>
         <p style="text-align: right;">Best regards</p>
     </body>
